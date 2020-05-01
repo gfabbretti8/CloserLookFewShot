336406aa275bd716278e6904d37ebe2bff676bf6
Giovanni Fabbretti <gfabbretti@MBP-di-Giovanni.lan>
2020-05-01 12:55:56 +0200

    u

--- train.py
+++ train.py
@@ -189,8 +189,8 @@ if __name__=='__main__':
             state = tmp['state_dict']
             state_keys = list(state.keys())
             for i, key in enumerate(state_keys):
-                if "layer\d{1}" in key:
-                    newkey = key.replace("layer[0-9]","trunk.").lower()  # an architecture model has attribute 'feature', load architecture feature to backbone by casting name from 'feature.trunk.xx' to 'trunk.xx'
+                if "layer[0-9]+" in key:
+                    newkey = key.replace("layer[0-9]+","trunk.").lower()  # an architecture model has attribute 'feature', load architecture feature to backbone by casting name from 'feature.trunk.xx' to 'trunk.xx'
                     state[newkey] = state.pop(key)
                 else:
                     state.pop(key)
